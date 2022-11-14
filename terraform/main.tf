19019a6f1dd5fc0fabda9a0184d0489028c4077c
z1nk <t.zink.business@outlook.com>
2022-11-14 17:26:51 +0100

    ready to rumble

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "google" {
-  project = "formal-vertex-364508" //TODO: Move project ID to a variable and find out how to use that.
+  project = var.project_id //TODO: Move project ID to a variable and find out how to use that.
   credentials = var.gcp_credentials
 }
 
@@ -22,7 +22,7 @@ resource "google_cloud_run_service" "default" {
   template {
     spec {
       containers {
-        image = "gcr.io/formal-vertex-364508/ccecaa-frontend:latest"
+        image = "gcr.io/${var.project_id}/ccecaa-frontend:latest"
       }
     }
   }
@@ -43,66 +43,66 @@ resource "google_cloud_run_service" "default" {
   }
 }*/
 
-# resource "google_cloud_run_service_iam_member" "default" {
-#   for_each = toset(data.google_cloud_run_locations.default.locations)
+resource "google_cloud_run_service_iam_member" "default" {
+  for_each = toset(var.regions)
 
-#   location = google_cloud_run_service.default[each.key].location
-#   project  = google_cloud_run_service.default[each.key].project
-#   service  = google_cloud_run_service.default[each.key].name
-#   role     = "roles/run.invoker"
-#   member   = "allUsers"
-# }
+  location = google_cloud_run_service.default[each.key].location
+  project  = google_cloud_run_service.default[each.key].project
+  service  = google_cloud_run_service.default[each.key].name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
 
 
-# resource "google_compute_region_network_endpoint_group" "default" {
-#   for_each = toset(data.google_cloud_run_locations.default.locations)
+resource "google_compute_region_network_endpoint_group" "default" {
+  for_each = toset(var.regions)
 
-#   name                  = "${var.name}--neg--${each.key}"
-#   network_endpoint_type = "SERVERLESS"
-#   region                = google_cloud_run_service.default[each.key].location
-#   cloud_run {
-#     service = google_cloud_run_service.default[each.key].name
-#   }
-# }
+  name                  = "neg-${each.key}"
+  network_endpoint_type = "SERVERLESS"
+  region                = google_cloud_run_service.default[each.key].location
+  cloud_run {
+    service = google_cloud_run_service.default[each.key].name
+  }
+}
 
-# module "lb-http" {
-#   source            = "GoogleCloudPlatform/lb-http/google//modules/serverless_negs"
-#   version           = "~> 6.0"
+module "lb-http" {
+  source            = "GoogleCloudPlatform/lb-http/google//modules/serverless_negs"
+  version           = "~> 6.0"
 
-#   project = var.project_id
-#   name    = var.name
+  project = var.project_id
+  name    = "lb-cloud-project"
 
-#   ssl                             = false
-#   managed_ssl_certificate_domains = []
-#   https_redirect                  = false
-#   backends = {
-#     default = {
-#       description            = null
-#       enable_cdn             = false
-#       custom_request_headers = null
+  ssl                             = false
+  managed_ssl_certificate_domains = []
+  https_redirect                  = false
+  backends = {
+    default = {
+      description            = null
+      enable_cdn             = false
+      custom_request_headers = null
 
-#       log_config = {
-#         enable      = true
-#         sample_rate = 1.0
-#       }
+      log_config = {
+        enable      = true
+        sample_rate = 1.0
+      }
 
-#       groups = [
-#         for neg in google_compute_region_network_endpoint_group.default:
-#         {
-#           group = neg.id
-#         }
-#       ]
+      groups = [
+        for neg in google_compute_region_network_endpoint_group.default:
+        {
+          group = neg.id
+        }
+      ]
 
-#       iap_config = {
-#         enable               = false
-#         oauth2_client_id     = null
-#         oauth2_client_secret = null
-#       }
-#       security_policy = null
-#     }
-#   }
-# }
+      iap_config = {
+        enable               = false
+        oauth2_client_id     = null
+        oauth2_client_secret = null
+      }
+      security_policy = null
+    }
+  }
+}
 
-/*output "url" {
+output "url" {
   value = "http://${module.lb-http.external_ip}"
-}*/
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,10 @@
+
+variable "project_id" {
+  description = "The ID of the project in which the resource belongs. The project must be a billing account project."
+  type        = string
+  default     = ["formal-vertex-364508"]
+}
+
 variable "gcp_credentials" {
   type = string
   sensitive = true
