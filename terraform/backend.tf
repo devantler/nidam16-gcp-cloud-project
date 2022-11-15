42a79f969f86cf620763a69b494a7556dc53b1d1
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2022-11-15 01:13:45 +0100

    pushed working example

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
-# resource "google_cloud_run_service" "default" {
+# resource "google_cloud_run_service" "backend" {
 #   for_each = var.regions
 
 #   name     = "backend-${each.value}"
 #   location = each.value
-
+    
 #   template {
 #     spec {
 #       containers {
--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -1,8 +1,9 @@
-resource "google_cloud_run_service" "default" {
+resource "google_cloud_run_service" "frontend" {
   for_each = toset(var.regions)
 
-  name     = "frontend-${each.value}"
-  location = each.value
+  name                       = "frontend-${each.value}"
+  autogenerate_revision_name = true
+  location                   = each.value
 
   template {
     spec {
@@ -11,4 +12,69 @@ resource "google_cloud_run_service" "default" {
       }
     }
   }
+}
+
+resource "google_cloud_run_service_iam_member" "frontend" {
+  for_each = toset(var.regions)
+
+  location = google_cloud_run_service.frontend[each.key].location
+  project  = google_cloud_run_service.frontend[each.key].project
+  service  = google_cloud_run_service.frontend[each.key].name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+resource "google_compute_region_network_endpoint_group" "frontend" {
+  provider = google-beta
+  for_each = toset(var.regions)
+
+  name                  = "neg-${each.key}"
+  network_endpoint_type = "SERVERLESS"
+  region                = google_cloud_run_service.frontend[each.key].location
+  cloud_run {
+    service = google_cloud_run_service.frontend[each.key].name
+  }
+}
+
+module "lb-http" {
+  source  = "GoogleCloudPlatform/lb-http/google//modules/serverless_negs"
+  version = "~> 6.0"
+
+  project = var.project_id
+  name    = "lb-cloud-project"
+
+  ssl                             = false
+  managed_ssl_certificate_domains = []
+  https_redirect                  = false
+  backends = {
+    default = {
+      description             = null
+      enable_cdn              = false
+      custom_request_headers  = null
+      custom_response_headers = null
+
+      log_config = {
+        enable      = true
+        sample_rate = 1.0
+      }
+
+      groups = [
+        for neg in google_compute_region_network_endpoint_group.frontend :
+        {
+          group = neg.id
+        }
+      ]
+
+      iap_config = {
+        enable               = false
+        oauth2_client_id     = null
+        oauth2_client_secret = null
+      }
+      security_policy = null
+    }
+  }
+}
+
+output "url" {
+  value = "http://${module.lb-http.external_ip}"
 }
