9d8b1fcaa704607414fa3123887a59df29d71a46
z1nk <t.zink.business@outlook.com>
2022-11-17 19:47:49 +0100

    terraform is vari gut now

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,10 @@ provider "google" {
 provider "google-beta" {
   project = var.project_id //TODO: Move project ID to a variable and find out how to use that.
 }
+
+terraform {
+ backend "gcs" {
+   bucket  = "f3c2369c2f1ea89d-bucket-tfstate"
+   prefix  = "terraform/state"
+ }
+}
