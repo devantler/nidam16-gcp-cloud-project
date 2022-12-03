52a91fa007996c576cb30709308909d4b7ae623a
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2022-12-03 19:26:59 +0100

    test

--- Frontend/src/common/Constants.ts
+++ Frontend/src/common/Constants.ts
@@ -1,6 +1,6 @@
 const constants = {
     serverBaseURL_development: 'http://localhost:3000',
-    serverBaseURL_production: 'http://localhost:3000'
+    serverBaseURL_production: 'https://backend-us-central1-u7hplahhma-uc.a.run.app'
 }
 
 export default constants;
