84bda926d2056ee91e2b47e71a2f2838f8d76fab
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2022-12-03 13:33:34 +0100

    Readded default project_id

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "project_id" {
   description = "The ID of the project in which the resource belongs. The project must be a billing account project."
   type        = string
-# default     = "formal-vertex-364508"
+  default     = "formal-vertex-364508"
 }
 
 variable "regions" {
