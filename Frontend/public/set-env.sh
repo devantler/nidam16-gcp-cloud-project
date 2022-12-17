0f34db24a834b2efff124b289e657d602ae884dd
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2022-12-17 14:06:57 +0100

    fix

--- Frontend/public/set-env.sh
+++ Frontend/public/set-env.sh
@@ -1,7 +1,7 @@
 cp -f /usr/share/nginx/html/index.html /tmp
 
-if [ -n "$REACT_APP_REGION" ]; then
-sed -i -e "s|REPLACE_REACT_APP_REGION|$REACT_APP_REGION|g" /tmp/index.html
+if [ -n "$REGION" ]; then
+sed -i -e "s|REPLACE_REACT_APP_REGION|$REGION|g" /tmp/index.html
 fi
 
 cat /tmp/index.html > /usr/share/nginx/html/index.html
