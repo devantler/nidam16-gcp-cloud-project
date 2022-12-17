e91b7f359c14147e530556d34f0de85b5edc4ed1
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2022-12-17 14:33:32 +0100

    fix

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -4,8 +4,8 @@ import bodyParser from 'body-parser';
 
 export const app = express()
 
-app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 
 let corsOptions;
 
@@ -15,8 +15,13 @@ if (process.env.NODE_ENV === 'development') {
   }
 } else {
   corsOptions = {
-    origin: ["*"]
+    origin: [
+      "https://backend-us-central1-u7hplahhma-uc.a.run.app",
+      "https://backend-europe-west1-u7hplahhma-ew.a.run.app",
+      "https://backend-europe-north1-u7hplahhma-lz.a.run.app",
+      "https://backend-asia-east1-u7hplahhma-de.a.run.app"
+    ]
   }
-} 
+}
 
-app.use(cors({origin: true}));
+app.use(cors(corsOptions));
